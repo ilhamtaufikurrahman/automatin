0af09f1492437bedeca7fac3f973c553df1522d2
Ilham Taufikurrahman <ilhamt35@gmail.com>
2021-09-02 05:58:05 +0700

    save bw

--- json.py
+++ json.py
@@ -4,29 +4,33 @@ import os
 import cv2
 
 rgbPath = "D:/Gambar/rgb/"
-jsonPath = "D:/Gambar/json/contoh.json"
+jsonPath = "D:/Gambar/json/"
 bwPath = "D:/Gambar/bw/"
 
+jsonFiles = os.listdir(jsonPath)
 #mengambil path rgb
 rgbFiles = os.listdir(rgbPath)
 #mengambil file rgb
 img0 = cv2.imread(os.path.join(rgbPath,rgbFiles[0]))
 #mengambil width dan height
-height, width = img0.shape[:2]
-
+height = img0.shape[0]
+width = img0.shape[1]
 #buka file json
-jsonFile = open(jsonPath)
+
+# print(jsonFiles)
+
+jsonf = open(os.path.join(jsonPath,jsonFiles[0]))
 
 point = []
 
 #parsing data json
-data = json.loads(jsonFile.read())
+data = json.loads(jsonf.read())
 
 #mengambil points yang ada di json
 for points in data['objects'][1]['points']['exterior']:
     point.append(points)
 
-print(point)
+# print(point)
 #menambahkan background
 img = np.zeros((height, width, 3), dtype = "uint8")
 
@@ -37,7 +41,10 @@ polygon = np.array(point, np.int32)
 polyImage = cv2.fillConvexPoly(img, polygon, (255,255,255))
 
 #menampilkan gambar
-cv2.imshow("example", polyImage)
+cv2.imshow("RGB", img0)
+cv2.imshow("BW", polyImage)
+
+cv2.imwrite(bwPath + "contoh.jpg", polyImage)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()

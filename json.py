5af4a43a2809cecddc0440fb8525455c6ebbaaa8
Ilham Taufikurrahman <ilhamt35@gmail.com>
2021-09-02 07:27:06 +0700

    penyempurnaan

--- json.py
+++ json.py
@@ -14,44 +14,50 @@ jsonFiles = os.listdir(jsonPath)
 rgbFiles = os.listdir(rgbPath)
 
 #mengambil file rgb
-img0 = cv2.imread(os.path.join(rgbPath,rgbFiles[0]))
+# img0 = cv2.imread(os.path.join(rgbPath,rgbFiles[0]))
 
-#mengambil width dan height
-height = img0.shape[0]
-width = img0.shape[1]
+# #mengambil width dan height
+# height = img0.shape[0]
+# width = img0.shape[1]
 
-for item in jsonFiles:
-    #menambahkan background
-    img = np.zeros((height, width, 3), dtype = "uint8")
-    # print(item)
+# for item in jsonFiles:
+#     #menambahkan background
+#     img = np.zeros((height, width, 3), dtype = "uint8")
+#     # print(item)
  
-    #buka file json
+#     #buka file json
+
+#     #parsing data json
+
+#     #mengambil points yang ada di json
+     
+#     point = []
+#     for points in data['objects'][1]['points']['exterior']:
+#         point.append(points)
+
+#     #membuat polygon dari points
+#     polygon = np.array(point, np.int32)
+
+#     #membuat polygon warna putih dalam background gambar
+#     polyImage = cv2.fillConvexPoly(img, polygon, (255,255,255))
+
+#     #menampilkan gambar
+#     # cv2.imshow("RGB", img0)
+#     # cv2.imshow("BW", polyImage)
+
+#     #memberi nama untuk bw jpg, dengan mengurangi 5 huruf terakhir dari json file
+#     bwFile = item[:-5]
+
+#     cv2.imwrite(bwPath + bwFile, img)
+
+#     print(bwFile + " berhasil ditambahkan")
+for item in jsonFiles:
     jsonf = open(os.path.join(jsonPath,item))
-
-    #parsing data json
     data = json.loads(jsonf.read())
-
-    #mengambil points yang ada di json
-    point = []
-    # for points in data['objects'][1]['points']['exterior']:
-    #     point.append(points)
-
-    # #membuat polygon dari points
-    # polygon = np.array(point, np.int32)
-
-    # #membuat polygon warna putih dalam background gambar
-    # polyImage = cv2.fillConvexPoly(img, polygon, (255,255,255))
-
-    #menampilkan gambar
-    # cv2.imshow("RGB", img0)
-    # cv2.imshow("BW", polyImage)
-
-    #memberi nama untuk bw jpg, dengan mengurangi 5 huruf terakhir dari json file
-    bwFile = item[:-5]
-
-    cv2.imwrite(bwPath + bwFile, img)
     
-    print(bwFile + " berhasil ditambahkan")
+    print(len(data['objects']))
+    
+    
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
